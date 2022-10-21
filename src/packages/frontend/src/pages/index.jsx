55acfed9f06126c6c09dce2ad6c15be652eb754d
nagasaiabhinay <nagasai.abhinay@gmail.com>
2022-10-21 11:14:27 +0530

    Frontend: Added Zustand State Management

--- src/packages/frontend/src/pages/index.jsx
+++ src/packages/frontend/src/pages/index.jsx
@@ -1,10 +1,13 @@
 import { Button } from '@mantine/core';
+import { useGlobalStore } from '@store/index';
 
 export default function HomeScreen() {
+    const loading = useGlobalStore((s) => s.loading);
+
     return (
         <div>
             <text className={` bps-font-bold bps-text-3xl`}>HomeScreen</text>
-            <Button>Hello Mantine</Button>
+            <Button loading={loading}>Hello Mantine</Button>
         </div>
     );
 }
