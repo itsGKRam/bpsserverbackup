ee2fbdf95c7e5d3bfc2142c02230f8d0bcd4782f
nagasaiabhinay <nagasai.abhinay@gmail.com>
2022-10-21 12:18:18 +0530

    Frontend: Added swr

--- src/packages/frontend/src/pages/_app.jsx
+++ src/packages/frontend/src/pages/_app.jsx
@@ -1,3 +1,4 @@
+import Axios from '@axios/index';
 import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { NotificationsProvider } from '@mantine/notifications';
@@ -7,6 +8,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
     const [colorScheme, setColorScheme] = React.useState('dark');
+    const api = Axios.init();
     const toggleColorScheme = (value) =>
         setColorScheme(value || (colorScheme === 'light' ? 'dark' : 'light'));
     const getLayout = Component.getLayout || ((page) => page);
@@ -16,26 +18,36 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>Bus Pass System</title>
             </Head>
-            <ColorSchemeProvider
-                colorScheme={colorScheme}
-                toggleColorScheme={toggleColorScheme}
+            <SWRConfig
+                value={{
+                    revalidateIfStale: false,
+                    revalidateOnFocus: true,
+                    revalidateOnReconnect: false,
+                    fetcher: (url) =>
+                        api.global.get(url).then((res) => res.data),
+                }}
             >
-                <MantineProvider
-                    withGlobalStyles
-                    withNormalizeCSS
-                    theme={{
-                        colorScheme,
-                    }}
+                <ColorSchemeProvider
+                    colorScheme={colorScheme}
+                    toggleColorScheme={toggleColorScheme}
                 >
-                    <NotificationsProvider>
-                        <ModalsProvider>
-                            <div className=' bps-flex bps-flex-col bps-relative bps-w-screen bps-h-screen'>
-                                {getLayout(<Component {...pageProps} />)}
-                            </div>
-                        </ModalsProvider>
-                    </NotificationsProvider>
-                </MantineProvider>
-            </ColorSchemeProvider>
+                    <MantineProvider
+                        withGlobalStyles
+                        withNormalizeCSS
+                        theme={{
+                            colorScheme,
+                        }}
+                    >
+                        <NotificationsProvider>
+                            <ModalsProvider>
+                                <div className=' bps-flex bps-flex-col bps-relative bps-w-screen bps-h-screen'>
+                                    {getLayout(<Component {...pageProps} />)}
+                                </div>
+                            </ModalsProvider>
+                        </NotificationsProvider>
+                    </MantineProvider>
+                </ColorSchemeProvider>
+            </SWRConfig>
         </>
     );
 }
