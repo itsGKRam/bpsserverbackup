d028a4293a3fd74f1c40078361608f1a24e8f215
nagasaiabhinay <nagasai.abhinay@gmail.com>
2022-10-21 16:36:48 +0530

    Frontend: Added Responsive RootLayout

--- src/packages/frontend/src/pages/index.jsx
+++ src/packages/frontend/src/pages/index.jsx
@@ -1,5 +1,10 @@
 import Home from '@components/home';
+import RootLaylot from '@components/layouts/rootLayout';
 
 export default function HomeScreen() {
     return <Home />;
 }
+
+HomeScreen.getLayout = function getLayout(page) {
+    return <RootLaylot>{page}</RootLaylot>;
+};
